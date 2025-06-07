d52f39949efb0b7ec6285a4dbc59e52a6e6b058a
B SAI HARI KRISHNA <166925264+BSHK57@users.noreply.github.com>
2025-06-07 18:28:05 +0530

    added

--- evoting/settings.py
+++ evoting/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -125,6 +126,8 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Media files (User uploaded content)
 # https://docs.djangoproject.com/en/4.2/topics/files/
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
